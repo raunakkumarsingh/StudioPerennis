4dd64ca4a7229c25f51b156f5dc21669c4cb13dd
raunakkumarsingh <35128126+raunakkumarsingh@users.noreply.github.com>
2024-03-29 21:50:09 +0530

    first-commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+// import logo from '../../assets/icons/logo1.png';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="Navbar fixed-top">
+      <Link to="/">
+        <div className="nav-logo strong-text mx-4">
+          {/* <img  src="" className="logo rounded-circle" alt=""  /> */}
+          StudioPerennis
+        </div>
+      </Link>
+      <div className={`nav-items ${isOpen ? "open" : ""} d-flex `}>
+        <Link onClick={() => setIsOpen(!isOpen)}  to="/home"><strong>Home</strong></Link>
+        <Link onClick={() => setIsOpen(!isOpen)}  to="/home"><strong>About</strong></Link>
+        <Link onClick={() => setIsOpen(!isOpen)} to="/vision"><strong>courses</strong></Link>
+        <Link onClick={() => setIsOpen(!isOpen)} to="/contact"><strong>Blog</strong></Link>
+      </div>
+      <div className={`nav-toggle ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css'; 
+
+function Home() {
+  return (
+    <div className="container d-flex flex-column justify-content-center align-items-center vh-100">
+      <h1 className="text-center">Studio Perennis</h1>
+      <h6 className="text-center">Developing Intelligence</h6>
+    </div>
+  );
+}
+
+export default Home;
